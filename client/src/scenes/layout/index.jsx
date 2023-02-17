f045e2a10529f8a7036dc53a6840ed46dd38a6cf
filimonkd <filimonkd@gmail.com>
2023-02-17 02:21:16 -0800

    adding Navbar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <Box width="50%" height="50%">
+      
+      <Box >
+        <Navbar/>
+        <Outlet />
+      </Box>
+    </Box>
   )
 }
 
