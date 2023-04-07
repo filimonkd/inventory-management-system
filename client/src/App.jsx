164ff963f8ce5683cc51e09647d3e76161c7f1c6
filimonkd <filimonkd@gmail.com>
2023-04-07 14:05:07 +0300

    signin ui and custom css

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import SignIn from  './pages/Authentication/SignIn'
+
+const App = () => {
+  const [loading, setLoading] = useState(true)
+
+  const preloader = document.getElementById('preloader')
+
+  if(preloader) {
+    setTimeout(() => {
+      preloader.style.display = 'none'
+      setLoading(false)
+    }, 2000);
+  }
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000)
+  }, [])
+
+  return (
+    !loading && (
+      <>
+        <Routes>
+          <Route exact path='/' element={<SignIn />} />
+        </Routes>
+      </>
+    )
+  )
+}
+
+export default App
